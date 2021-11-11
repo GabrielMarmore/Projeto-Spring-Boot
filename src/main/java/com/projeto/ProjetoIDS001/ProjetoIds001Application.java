432418c6648c31bc8c533aff9928e38a8aafbb0f
Gabriel Marmore <gabrielpiraja@hotmail.com>
2021-11-11 20:49:11 -0300

    first commit

---
+++ src/main/java/com/projeto/ProjetoIDS001/ProjetoIds001Application.java
@@ -0,0 +1,13 @@
+package com.projeto.ProjetoIDS001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIds001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIds001Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/ProjetoIDS001/ProjetoIds001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.ProjetoIDS001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIds001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
